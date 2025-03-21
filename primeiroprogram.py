c44c4d6a6ec34581ec67317d560c5b8dd17d2ce2
DAVID RUAN PEREIRA DE SOUZA <202404116051@alunos.unifanor.edu.br>
2025-03-21 10:18:44 -0300

    Atualização

---
+++ primeiroprogram.py
@@ -0,0 +1,14 @@
+# Criação e declaração de variaveis e estrutura de emtrada e saída de dados e laços de repetição
+nome = "David Ruan"
+numeroMatricula = 202404116051
+print(f"O teu nome é {nome} e a tua matricula é {numeroMatricula} ")
+
+
+'''multiplo = 0
+valorUser = int(input("Digite o número que deseja multiplicar: "))
+resultado = 0
+
+while multiplo <= 9:
+    multiplo = multiplo +1
+    resultado = multiplo*valorUser
+    print(f"{valorUser} X {multiplo} = {resultado}")'''
